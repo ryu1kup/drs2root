2e5047b517e2f17bd27d0637a901aef60a897523
Ryuichi Ueno <ryu1kup@gmail.com>
2020-11-13 05:42:43 +0900

    use std::array instead of c-style array

--- drs2root.cpp
+++ drs2root.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <array>
 #include <string>
 #include <cassert>
 #include <filesystem>
@@ -16,27 +17,23 @@ void convert_drs4(const std::string inputfile, const std::string& outputfile){
     auto f = new TFile(outputfile.c_str(), "recreate");
     auto t = new TTree("tree", "DRS4 waveform data");
 
-    std::uint16_t buf_event1[9];
-    std::uint16_t buf_event2[3];
-    std::uint16_t buf_event3[2];
+    std::array<std::uint16_t, 9> buf_event1;
+    std::array<std::uint16_t, 3> buf_event2;
+    std::array<std::uint16_t, 2> buf_event3;
 
     std::uint16_t range_center;
     std::uint16_t trigger_cell;
-    float ch1_dt[1024];
-    float ch2_dt[1024];
-    float ch3_dt[1024];
-    float ch4_dt[1024];
-    std::uint16_t ch1_wf[1024];
-    std::uint16_t ch2_wf[1024];
-    std::uint16_t ch3_wf[1024];
-    std::uint16_t ch4_wf[1024];
+    std::array<float, 1024> ch1_dt {};
+    std::array<float, 1024> ch2_dt {};
+    std::array<float, 1024> ch3_dt {};
+    std::array<float, 1024> ch4_dt {};
+    std::array<std::uint16_t, 1024> ch1_wf {};
+    std::array<std::uint16_t, 1024> ch2_wf {};
+    std::array<std::uint16_t, 1024> ch3_wf {};
+    std::array<std::uint16_t, 1024> ch4_wf {};
 
     t->Branch("range_center", &range_center);
     t->Branch("trigger_cell", &trigger_cell);
-    t->Branch("ch1_wf", &ch1_wf, "ch1_wf[1024]/s");
-    t->Branch("ch2_wf", &ch2_wf, "ch2_wf[1024]/s");
-    t->Branch("ch3_wf", &ch3_wf, "ch3_wf[1024]/s");
-    t->Branch("ch4_wf", &ch4_wf, "ch4_wf[1024]/s");
 
     bool has_ch1 = false;
     bool has_ch2 = false;
@@ -55,15 +52,19 @@ void convert_drs4(const std::string inputfile, const std::string& outputfile){
         ifs.read(reinterpret_cast<char *>(&header), sizeof(header));
         if (header[0] == 'C' && header[1] == '0' && header[2] == '0' && header[3] == '1') {
             has_ch1 = true;
+            t->Branch("ch1_wf", &ch1_wf);
             ifs.read(reinterpret_cast<char *>(&ch1_dt), sizeof(ch1_dt));
         } else if (header[0] == 'C' && header[1] == '0' && header[2] == '0' && header[3] == '2') {
             has_ch2 = true;
+            t->Branch("ch2_wf", &ch2_wf);
             ifs.read(reinterpret_cast<char *>(&ch2_dt), sizeof(ch2_dt));
         } else if (header[0] == 'C' && header[1] == '0' && header[2] == '0' && header[3] == '3') {
             has_ch3 = true;
+            t->Branch("ch3_wf", &ch3_wf);
             ifs.read(reinterpret_cast<char *>(&ch3_dt), sizeof(ch3_dt));
         } else if (header[0] == 'C' && header[1] == '0' && header[2] == '0' && header[3] == '4') {
             has_ch4 = true;
+            t->Branch("ch4_wf", &ch4_wf);
             ifs.read(reinterpret_cast<char *>(&ch4_dt), sizeof(ch4_dt));
         } else {
             ifs.seekg(-sizeof(header), std::ios::cur);
